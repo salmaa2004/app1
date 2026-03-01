6f08234a55c0c85eadd9a298565c551fda3d2a54
salma mahmoud <salmahegzy@gmail.com>
2026-03-02 00:14:28 +0200

    Q3

--- app1/Program.cs
+++ app1/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            #region Q1
+            //Console.WriteLine("Q1: #region and #endregion are used to organize code.");
+            //Console.WriteLine("They allow collapsing and expanding sections in Visual Studio.");
+            #endregion
+
+
+            #region Q2
+            //int explicitNumber = 10;     
+            //var implicitNumber = 20;     
+            //Console.WriteLine(" Explicit = {explicitNumber}, Implicit = {implicitNumber}\n");
+            #endregion
+
+            #region Q3
+            //const double PI = 3.14159;
+            //Console.WriteLine($"Q3: Constant PI = {PI}");
+            //Console.WriteLine("Constants cannot be changed after declaration.\n");
+
+            #endregion
         }
     }
 }
