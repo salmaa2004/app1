0eeac6f2f3e180efce8c8a9c8af5a574655c9c9f
salma mahmoud <salmahegzy@gmail.com>
2026-03-02 00:21:08 +0200

    Q2

--- app1/Program.cs
+++ app1/Program.cs
@@ -8,9 +8,14 @@
             //Console.WriteLine("Q1: #region and #endregion are used to organize code.");
             //Console.WriteLine("They allow collapsing and expanding sections in Visual Studio.");
             #endregion
+            #region Q2
+            //int explicitNumber = 10;     
+            //var implicitNumber = 20;     
+            //Console.WriteLine(" Explicit = {explicitNumber}, Implicit = {implicitNumber}\n");
+            #endregion
 
+          
 
-        
         }
     }
 }
